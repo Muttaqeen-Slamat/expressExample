b01b0e6a40985751970b72c020ef8deaf8eac70e
Muttaqeen-Slamat <muttaqeens@gmail.com>
2024-02-12 12:44:09 +0200

    update

--- index.js
+++ index.js
@@ -2,11 +2,14 @@ import express from 'express'
 
 // Create an express app
 const app = express()
+const router = express.Router()
+app.use(router)
 const port = +process.env.PORT || 4000
 
 // router
 //.httpmethod you want to use 
-app.get('/', (req, res)=>{
+// ('^/$|/express') for home to be displayed on both home and custom name for home 
+router.get('/', display, (req, res)=>{
     // res.status(200).json({
     //     msg: "You're home"
     // })
@@ -16,8 +19,14 @@ app.get('/', (req, res)=>{
     })
 })
 
+//middleware
+function display(req,res,next){
+    console.log('Hello there');
+    next()
+   //next() commented out will let the page reload, next hence then display results
+}
 
-app.get('/about', (req, res)=>{
+router.get('/about', (req, res)=>{
     res.json({
             status: res.statusCode,
             msg: "About page"    
@@ -25,11 +34,11 @@ app.get('/about', (req, res)=>{
 })
 
 
-app.all('*', (req, res)=>{
-    res.json({
-            status: 404,
-            msg: "404 page"    
-    })
-})
+// app.all('*', (req, res)=>{
+//     res.json({
+//             status: 404,
+//             msg: "404 page"    
+//     })
+// })
 
 app.listen(port)
